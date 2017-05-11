fa9c545396d90b979dac8d8af2965f72df9a842a
ptgc <petter.christie@gmail.com>
2017-05-11 18:16:44 +0200

    CDY test commit

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello CDU with CI from Git!</h1>');
+    res.end('<h1>Hello CDU test with CI from Git!</h1>');
 }).listen(process.env.PORT);
